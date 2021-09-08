11fa89f5fb08354f12b3d7aca1514315b1369332
Ivan Khoda <ivankhoda@gmail.com>
2021-09-08 15:50:03 +0300

    Fixing expired images

--- src/data/data.ts
+++ src/data/data.ts
@@ -3,7 +3,7 @@ const foods = [
     id: "0",
     name: "Lyn's milktea",
     image:
-      "https://scontent.fhel5-1.fna.fbcdn.net/v/t1.0-9/153823573_173444617915389_531446805344180305_n.jpg?_nc_cat=110&ccb=3&_nc_sid=8bfeb9&_nc_ohc=TFgv83TETJUAX9ag-cs&_nc_ht=scontent.fhel5-1.fna&oh=ea9641c375ce2294f0c817ed68fa95e9&oe=605E94DC",
+      "https://images.unsplash.com/photo-1572490122747-3968b75cc699?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80",
     description: "Sarap, lamig, tamis",
     price: 50.0,
     amount: 1,
@@ -12,7 +12,7 @@ const foods = [
     id: "1",
     name: "Chease Overlord",
     image:
-      "https://scontent.fhel5-1.fna.fbcdn.net/v/t1.0-9/153633575_173445014582016_5404520069511860289_n.jpg?_nc_cat=101&ccb=3&_nc_sid=8bfeb9&_nc_ohc=--E212_WshUAX8S6CCF&_nc_ht=scontent.fhel5-1.fna&oh=41be36a7f75be5ac799b582e0c6ef35c&oe=605F743F",
+      "https://images.unsplash.com/photo-1508736793122-f516e3ba5569?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=666&q=80",
     description: "The most cheesy version of fries",
     price: 60.0,
     amount: 1,
@@ -21,7 +21,7 @@ const foods = [
     id: "2",
     name: "Long dog",
     image:
-      "https://scontent.fhel5-1.fna.fbcdn.net/v/t1.0-9/152846797_173444857915365_3694295647213876721_n.jpg?_nc_cat=110&ccb=3&_nc_sid=8bfeb9&_nc_ohc=b7P64n3cT8IAX_DfDQK&_nc_ht=scontent.fhel5-1.fna&oh=de3c898ba87cf45e95881650c3443747&oe=605EEE33",
+      "https://images.unsplash.com/photo-1613482084286-41f25b486fa2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1567&q=80",
     description: "Sarap, lamig, tamis",
     price: 70.0,
     amount: 1,
@@ -30,7 +30,7 @@ const foods = [
     id: "3",
     name: "Lyn's Burger",
     image:
-      "https://scontent.fhel5-1.fna.fbcdn.net/v/t1.0-9/148280861_164796715446846_3690810306950105271_n.jpg?_nc_cat=109&ccb=3&_nc_sid=8bfeb9&_nc_ohc=DUDbJTmasqUAX9Fhmus&_nc_ht=scontent.fhel5-1.fna&oh=208c4b1078fec2b1db207d6ab10fe838&oe=605E415D",
+      "https://images.unsplash.com/photo-1565299507177-b0ac66763828?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=964&q=80",
     description: "Classics",
     price: 50.0,
     amount: 1,
